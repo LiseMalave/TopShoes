5c28f7ec5e35cf3339cbe50b9587688c90780a38
LiseMalave <malave.lopez9@gmail.com>
2024-07-15 22:57:12 -0300

    creando las routes2

---
+++ TopShoes/src/components/About/About.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.div`
+  display: flex;
+  line-height: 25px;
+  justify-content: center;
+  gap: 30px;
+  padding: 2rem;
+  background-color: bisque;
+`
+
+export const AboutInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #18191a;
+  gap: 15px;
+  width: 20%;`
--- TopShoes/src/components/About/About.jsx
+++ TopShoes/src/components/About/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import { AboutContainer, AboutInfo } from './About'
 function About() {
   return (
     
-    <h2>Top Shoes es una Fábrica de Calzado que elabora zapatos para damas, niñas y bebés. Presenta una gama de modelos con una excelente calidad en el Calzado</h2>
+   <AboutContainer>
+    <AboutInfo>
+      <h2>Top Shoes es una Fábrica de Calzado que elabora zapatos para damas, niñas y bebés. Presenta una gama de modelos con una excelente calidad en el Calzado</h2>
+    </AboutInfo>
+    
+    </AboutContainer>
   )
 }
 
